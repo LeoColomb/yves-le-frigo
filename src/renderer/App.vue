bc8ac75c522ec306add452d7f4bdd67d2a75af51
Léo Colombaro <git@colombaro.fr>
2017-11-22 01:03:24 +0100

    init with SimulatedGREG/electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'yves-le-frigo'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>

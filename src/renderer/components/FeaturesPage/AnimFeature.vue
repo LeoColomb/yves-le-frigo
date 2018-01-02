b30b9b5e20304d68909f7788bb7b43ae9b91d6da
Léo Colombaro <git@colombaro.fr>
2018-01-02 23:58:16 +0100

    Nœud de cravate Ref https://trello.com/c/5a1436f19979143f1d2a6153

---
+++ src/renderer/components/FeaturesPage/AnimFeature.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cont">
+    <img :src="`static/features/anim/${this.modifier}.gif`">
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'anim-feature',
+
+    computed: mapState({
+      modifier: state => state.feature.modifier
+    })
+  }
+</script>
+
+<style scoped>
+  .cont {
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: space-around;
+    height: 100vh;
+    width: 100%;
+    background: black;
+  }
+
+  .cont img {
+    max-height: 100vh;
+  }
+
+</style>
